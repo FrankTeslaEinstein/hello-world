2241c32fab8c7745edc5519a44cf75ad91f01d86
Frank Tesla Einstein <fjtesla@gmail.com>
2020-02-20 13:36:18 -0600

    Mi primer commint, desde linea de comandos

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ void leer(char msg[]);
 
 int main(void){
 	// comentario para git...
+	// de nuevo es otro comentario
+
 	create_UDP (&canal, "192.168.43.94", 8080, "192.168.43.122", 1112);
 	//create_UDP (&canal, "192.168.100.4", 8080, "192.168.100.90", 1112);
 	// inicializando canal UDP
